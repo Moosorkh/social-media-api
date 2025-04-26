f0f9b4935ae138f5f4b50135ae3ef91add89b32b
fortis <moosorkh@gmail.com>
2025-04-25 23:56:37 -0700

    reduced duplication and practiced DRY second commit

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -3,11 +3,6 @@ import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { BaseUserDto } from '../../users/dto/base-user.dto'
 
 export class CreateUserDto extends BaseUserDto {
-  @ApiProperty({ description: 'Username of the user' })
-  @IsNotEmpty()
-  @IsString()
-  username: string;
-
   @ApiProperty({ description: 'Password of the user' })
   @IsNotEmpty()
   @IsString()
